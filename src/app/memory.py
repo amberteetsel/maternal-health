548f3ecadad441003768ecabdb610f8cd879f6fc
amberteetsel <amte1696@colorado.edu>
2026-06-13 12:59:15 -0600

    bug fix

--- src/app/memory.py
+++ src/app/memory.py
@@ -10,7 +10,7 @@ def reduce_df_memory(df):
     float_cols = tmp.select_dtypes(include=['float']).apply(pd.to_numeric, downcast='float')
     tmp[float_cols.columns] = float_cols
 
-    int_cols = tmp.select_dtypes(include=['int']).apply(pd.to_numeric, downcast='int')
+    int_cols = tmp.select_dtypes(include=['int']).apply(pd.to_numeric, downcast='signed')
     tmp[int_cols.columns] = int_cols
 
     return tmp
