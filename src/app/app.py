42e6cac8471fd5f42e4e582189385a5b25e7d572
amberteetsel <amte1696@colorado.edu>
2026-06-17 21:58:14 -0600

    end clustering

--- src/app/app.py
+++ src/app/app.py
@@ -674,6 +674,7 @@ prep_md = inspect.cleandoc("""
     the DataFrame was melted into wide format.
     Category labels such as `State` and `Year` were stripped away to leave only unlabelled relevant features and values.
     Features were also reconstructed so that for all columns, a higher number indicated a "worse" outcome than a lower number.
+    For example, "Adequate Prenatal Care" was converted to "Inadequate Prenatal Care" by flipping the percentage.
     This increases interpretability of results. Z-Score standardization was also used so that varying data magnitudes would
     not have outsized influence on clusters.
 """)
